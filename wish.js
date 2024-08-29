c0cddf3cabc31e8b3cf03a242d9c2433096ef0be
Tayer <tayergustavo@hotmail.com>
2024-08-29 08:23:04 -0300

    teste

--- wish.js
+++ wish.js
@@ -1,25 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("entrou no dom")
+    (function (d, t) {
+        var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        v.onload = function () {
+            console.log("Script da Voiceflow carregado");
 
-(function (d, t) {
-    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-    v.onload = function () {
-        window.voiceflow.chat.load({
-            verify: { projectID: '66c7bab6ac85545518b9613f' },
-            url: 'https://general-runtime.voiceflow.com',
-            versionID: 'production',
-            /**
-* [optional] override configured agent definitions on integrations tab
-*/
-            assistant: {
-                title: "oie",
-                description: "teste",
-                // image: "string",
-                // color: "string",
-                // stylesheet: "string" //link to your self-hosted stylesheet
-            }
-        })
-    };
-    v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
-    v.type = "text/javascript";
-    s.parentNode.insertBefore(v, s);
-})(document, 'script');})
+            window.voiceflow.chat.load({
+                verify: { projectID: '66c7bab6ac85545518b9613f' },
+                url: 'https://general-runtime.voiceflow.com',
+                versionID: 'production',
+                /**
+    * [optional] override configured agent definitions on integrations tab
+    */
+                assistant: {
+                    title: "oie",
+                    description: "teste",
+                    // image: "string",
+                    // color: "string",
+                    // stylesheet: "string" //link to your self-hosted stylesheet
+                }
+            })
+        };
+        v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+        v.type = "text/javascript";
+        s.parentNode.insertBefore(v, s);
+    })(document, 'script');
+})
